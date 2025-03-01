c3500248face1b43e07a17dc78508ebea5d46e33
laass <thiagolaass202@gmail.com>
2025-03-01 17:56:33 -0300

    better code readability

--- LAB001/Backend/src/Middlewares/Exceptions/Exceptions.cs
+++ LAB001/Backend/src/Middlewares/Exceptions/Exceptions.cs
@@ -1,26 +1,14 @@
 namespace Backend.src.Middlewares.Exceptions
 {
-    public class NotFoundException(string message) : Exception(message)
-    {
-    }
+    public class NotFoundException(string message) : Exception(message) { }
 
-    public class BadRequestException(string message) : Exception(message)
-    {
-    }
+    public class BadRequestException(string message) : Exception(message) { }
 
-    public class UnauthorizedException(string message) : Exception(message)
-    {
-    }
+    public class UnauthorizedException(string message) : Exception(message) { }
 
-    public class KeyNotFoundException(string message) : Exception(message)
-    {
-    }
+    public class KeyNotFoundException(string message) : Exception(message) { }
 
-    public class InvalidPasswordException(string message) : Exception(message)
-    {
-    }
+    public class InvalidPasswordException(string message) : Exception(message) { }
 
-    public class InvalidOperationException(string message) : Exception(message)
-    {
-    }
+    public class InvalidOperationException(string message) : Exception(message) { }
 }
